5acc32a1dac1f0ba30d1337c4752d1f93e1b8d1d
Hugo Lewi Hammer <hugoh@oslomet.no>
2026-06-18 13:13:16 +0200

    Update synthetic_example.py

--- examples/synthetic_example.py
+++ examples/synthetic_example.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import torch
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
